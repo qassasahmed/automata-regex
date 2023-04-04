d05f80e8c4187d5d51ef301c8245b3bf3dc4c952
qassasahmed <qassas.ahmed@mau.edu.eg>
2023-04-04 07:00:00 +0200

    main.py added

---
+++ lab-6/main.py
@@ -0,0 +1,22 @@
+import re
+
+def validate_email(txt):
+    email_pattern = re.compile(r'^([\w.]+)@([\w.]+)\.([a-z.]{2,4})$', re.I)
+    email_pattern_x = re.compile(r"""
+    ^([\w.]+)         #start with username
+    @                 #literal @
+    ([\w.]+)          #domain name
+    \.                #literal dot
+    ([a-z.]{2,4})$    #ends with top level domain
+    """, re.X | re.I)
+    return email_pattern_x.fullmatch(txt)
+
+if __name__ == '__main__':
+    e = validate_email('qassas.ahmed@mau.edu.eg')
+    if(e):
+        print(f'hostname: {e.group(1)}')
+        print(f'domain name: {e.group(2)}')
+        print(f'top level domain: {e.group(3)}')
+    else: 
+        print('None: no match object')
+
