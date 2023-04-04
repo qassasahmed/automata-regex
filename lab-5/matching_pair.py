4da3240e6881e92608d21cdb3520dfa7d93c34d2
qassasahmed <qassas.ahmed@mau.edu.eg>
2023-04-04 12:28:30 +0200

    lab 5 and 6

--- lab-5/matching_pair.py
+++ lab-5/matching_pair.py
@@ -1,11 +1,23 @@
+#sub
+#greedy
 import re
 
 
 def pair(txt):
-	for m in re.finditer(r'\b(\w+)\s?\1', txt, re.I):
-		print(f'{m.start()}-{m.end()}: {m.group()}')
+	"""
+	this funciton finds any repeated word
+	"""
+	for m in re.finditer(r'\b(\w+?)\s?\1+', txt, re.I):
+		print(f'{m.start()}-{m.end()}: {m.group(1)}')
 
 
-if __name__ == '__main__':
-	text = 'He he was carefully disguised butbut captured quickly by by police.'
-	pair(text)
+def autoCorrect(repeated):
+	return repeated.group(1)
+
+
+if __name__ == "__main__":
+	txt = "the thethethe quick brown fox jumps overoveroverover the lazy dog"
+	pattern = re.compile(r'\b(\w+?)\s?\1+', re.I)
+	x = pattern.subn(string=txt, repl=autoCorrect)
+	print(x)
+	pair(txt)
--- lab-6/main.py
+++ lab-6/main.py
@@ -5,25 +5,23 @@ import re
 
 
 def validate_email(txt):
-    email_pattern = re.compile(r'^([\w.]+)@([\w.]+)\.([a-z.]{2,4})$', re.I)
-    email_pattern_x = re.compile(r"""
-    ^([\w.]+)         #start with username
-    @                 #literal @
-    ([\w.]+)          #domain name
-    \.                #literal dot
-    ([a-z.]{2,4})$    #ends with top level domain
-    """, re.X | re.I)
-    return email_pattern_x.fullmatch(txt)
+    email_pattern = re.compile(r"""
+    ^([\w.]+) #hostname
+    @ #literal @
+    ([\w.]+) #domain name
+    \. #literal dot
+    ([a-z.]{2,5}) #top level domain
+    """, re.X | re.I | re.M)
+    return email_pattern.search(txt)
 
 
 if __name__ == '__main__':
-    e = validate_email('qassas.ahmed@mau.edu.eg')
+    e = validate_email('blueh blueh\nqassas.ahmed@mau.edu.eg')
     if(e):
         print(f'email: {e.group(0)}')
         print(f'hostname: {e.group(1)}')
         print(f'domain name: {e.group(2)}')
         print(f'top level domain: {e.group(3)}')
-        print(f'all: {e.group(1,2, 3)}')
+        print(f'all: {e.group(1, 2, 3)}')
     else: 
         print('None: no match object')
-
